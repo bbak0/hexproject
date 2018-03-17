1ed6eaebcfc216cd5db0a446404d54c62eb046a2
bbak0 <b_bak@outlook.com>
2018-03-17 14:14:43 +0100

    dupex

--- hex/urls.py
+++ hex/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
-    path('login/', views.login_view, name='login_view'),   
+    path('login/', views.login_view, name='login_view'),
+    path('event/<int:event_id>', views.event_view, name='event_view'),
+    path('feed/', views.feed, name='feed')
 ]
