7a0dc100d200ac0761a096fbf150e014f1875953
bbak0 <b_bak@outlook.com>
2018-03-17 05:30:07 +0100

    fix 3

--- hex/views.py
+++ hex/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
 
 def index(request):
-    render('dupex hehe')
+    return HttpResponse("dupex hehe")
