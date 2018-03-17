f0ff55f97403091f626e2e48a170863177f58d2a
bbak0 <b_bak@outlook.com>
2018-03-17 04:20:45 +0100

    database setup

--- hex/models.py
+++ hex/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class events(models.Model):
-	date = models.DateField()
+class Events(models.Model):
+	date = models.DateTimeField()
 	x_coordinate = models.DoubleField(null=True)
 	y_coordinate = models.DoubleField(null=True)
 	title = models.CharField(max_length = 50)
 	description = models.TextField()
-	organizer = models.ForeignKey()
+	organizer = models.ForeignKey(settings.AUTH_USER_MODEL)
