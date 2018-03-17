4b9ce01920bc619c0ef9c7855bf7df32b8b842e3
JoaoFerreira100 <32512196+JoaoFerreira100@users.noreply.github.com>
2018-03-17 04:39:08 +0100

    here we go

--- hex/models.py
+++ hex/models.py
@@ -1,9 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-class events(models.Model):
-	date = models.DateField()
-	x_coordinate = models.DoubleField(null=True)
-	y_coordinate = models.DoubleField(null=True)
-	title = models.CharField(max_length = 50)
-	description = models.TextField()
-	organizer = models.ForeignKey()
+PREFERENCES = (('KD', 'Children'),
+               ('EL', 'Elderly'),
+               ('HO', 'Homeless'),
+               ('TE', 'Tech'),
+               ('MU', 'Music'),
+               ('EV', 'Environmental'),
+               ('ED', 'Educational'),
+               )
+
+class Volunteer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    preferences = MultiSelectField(choices=PREFERENCES,
+                                 max_choices=7,
+                                 max_length=14)
+
+class Benefactor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+
+class Benefactor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
